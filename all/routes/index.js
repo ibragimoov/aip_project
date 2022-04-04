67bb075528d5bdbdd0f36dc307b62d31687879a1
Ibragimov Eldar <1ibragimov.e.i20@gmail.com>
2022-04-04 23:03:31 +0300

    add characters

--- all/routes/index.js
+++ all/routes/index.js
@@ -6,4 +6,28 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/rick', function(req, res, next) {
+  res.render('index', { title: 'Рик' });
+});
+
+router.get('/morty', function(req, res, next) {
+  res.render('index', { title: 'Морти' });
+});
+
+router.get('/summer', function(req, res, next) {
+  res.render('index', { title: 'Саммер' });
+});
+
+router.get('/bet', function(req, res, next) {
+  res.render('index', { title: 'Бет' });
+});
+
+router.get('/jerry', function(req, res, next) {
+  res.render('index', { title: 'Джерри' });
+});
+
+router.get('/messeks', function(req, res, next) {
+  res.render('index', { title: 'Мистер Миссикс' });
+});
+
 module.exports = router;
