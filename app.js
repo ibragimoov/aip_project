387ef5325b693d15f7de95057bac6bc52ffca00d
Ibragimov Eldar <1ibragimov.e.i20@gmail.com>
2022-04-04 16:23:02 +0300

    body-parser

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
 import express from "express"
-var app = express()
+import bodyParser from 'body-parser'
 
-app.get("/", (req,res) => {
-    res.send("Серверная информация для браузера")
+var app = express()
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    var form = '<!doctype html>'+
+    '<html lang="ru">'+
+    '<head>'+
+    '<meta charset="UTF-8">'+
+    '<title>Форма</title>'+
+    '</head>'+
+    '<body>'+
+    '<h1>Форма для отправки данных на сервер</h1>'+
+    '<form action="/submit_result" method="post">'+
+    '<textarea name="text" id="" cols="30" rows="10"></textarea><br/>'+
+    '<input type="submit" value="Отправить данные на сервер"/>'+
+    '</form>'+
+    '</body>'+
+    '</html>'
+    res.send(form)
 })
 
-app.listen("3000")
+app.post("/submit_result", (req, res) => {
+    console.log(req.body)
+    var post_text = req.body.text ? "Вы отправили на сервер текст:"
+        + req.body.text : "Вы отправили на сервер пустую строку"
+    res.send(post_text)
+})
+    
+
+app.listen("3000", () => {
+    console.log('Сервер работает и слушает порт 3000')
+})
