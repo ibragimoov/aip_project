c95794049ed56fc88aa66a8179a7a84bfa0c9cba
Ibragimov Eldar <1ibragimov.e.i20@gmail.com>
2022-04-19 08:16:02 +0300

    schema

--- all/createDB.js
+++ all/createDB.js
@@ -12,12 +12,14 @@
 
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/test')
-var Cat = mongoose.model('Cat', { name: String })
+var schema = mongoose.Schema({ name: String })
+
+schema.methods.meow = function(){
+    console.log(this.get("name") + " сказал мяу")
+}
+
+var Cat = mongoose.model('Cat', schema)
 var kitty = new Cat({ name: 'Пушок' })
 kitty.save(function (err) {
-    if (err) {
-    console.log(err)
-    } else {
-    console.log('Мяу')
-    }
+kitty.meow()
 })
