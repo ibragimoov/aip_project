81bf7e965adaa74785f63f43dd0526ead83a482f
Ibragimov Eldar <90416969+ibragimoov@users.noreply.github.com>
2022-06-24 18:19:54 +0300

    getIndexes()

--- all/createDB.js
+++ all/createDB.js
@@ -1,8 +1,17 @@
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/all')
-var Hero = require("./models/hero").Hero
-var async = require('async');
-var data = require('./data.js').data;
+var async = require("async")
+var data = require('./data.js').data
+
+async.series([
+        open,
+        dropDatabase,
+        requireModels,
+        createHeroes
+    ],
+    function(err,result){
+        mongoose.disconnect()
+    })
 
 function open(callback){
     mongoose.connection.on("open",callback)
@@ -15,23 +24,18 @@ function dropDatabase(callback){
 
 function createHeroes(callback){
     async.each(data, function(heroData, callback){
-           var hero = new mongoose.models.Hero(heroData);
-            hero.save(callback);
+            var hero = new mongoose.models.Hero(heroData)
+            hero.save(callback)
         },
-        callback);
-};
-
-function close(callback){
-    mongoose.disconnect(callback)
+        callback)
 }
 
-async.series([
-    open,
-    dropDatabase,
-    createHeroes,
-    close
-],
-function(err,result){
-    if(err) throw err
-    console.log("ok")
-})
+function requireModels(callback){
+    require("./models/hero").Hero
+
+    async.each(Object.keys(mongoose.models),function(modelName){
+        mongoose.models[modelName].ensureIndexes(callback)
+    },
+        callback
+    )
+}
