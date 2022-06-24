d10954375fbbd78d99beb55c7556be72cf5903a2
Ibragimov Eldar <90416969+ibragimoov@users.noreply.github.com>
2022-06-24 20:16:52 +0300

    data menu from mongo

--- all/routes/heroes.js
+++ all/routes/heroes.js
@@ -1,23 +1,35 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 var Hero = require("../models/hero").Hero
+var async = require("async")
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Новый маршрутизатор, для маршрутов, начинающихся с heroes');
+    res.send('Новый маршрутизатор, для маошрутов, начинающихся с heroes')
 });
 
 /* Страница героев */
 router.get('/:nick', function(req, res, next) {
-    Hero.findOne({nick:req.params['nick']}, function(err,hero){
-        if(err) return next(err)
-        if(!hero) return next(new Error("Нет такого героя"))
-        res.render('hero', {
-            title: hero.title,
-            picture: '/' + hero.avatar,
-            desc: hero.desc
+    async.parallel([
+            function(callback){
+                Hero.findOne({nick:req.params.nick}, callback)
+            },
+            function(callback){
+                Hero.find({},{_id:0,title:1,nick:1},callback)
+            }
+        ],
+        function(err,result){
+            if(err) return next(err)
+            var hero = result[0]
+            var heroes = result[1] || []
+            if(!hero) return next(new Error("Нет такого героя в этой книжке"))
+            res.render('hero', {
+                title: hero.title,
+                picture: '/' + hero.avatar,
+                desc: hero.desc,
+                menu: heroes
+            });
         })
-    })
 })
 
-module.exports = router;
+module.exports = router
