888a003f1343971609031d873d5074fa9d8cb6cf
Ibragimov Eldar <90416969+ibragimoov@users.noreply.github.com>
2022-06-24 21:36:26 +0300

    add users to mogno

--- all/routes/index.js
+++ all/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var router = express.Router()
 var Hero = require("../models/hero").Hero
+var User = require("../models/User").User
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -17,11 +18,30 @@ router.get('/logreg', function(req, res, next) {
     res.render('logreg',{title: 'Вход'});
 });   
 
+/* POST login/registration page. */
+router.post('/logreg', function(req, res, next) {
+    var username = req.body.username
+    var password = req.body.password
+    User.findOne({username:username},function(err,user){
+        if(err) return next(err)
+        if(user){
+            if(user.checkPassword(password)){
+                req.session.user = user._id
+                res.redirect('/')
+            } else {
+                res.render('logreg', {title: 'Вход'})
+            }
+        } else {
+        var user = new User({username:username,password:password})
+        user.save(function(err,user){
+            if(err) return next(err)
+            req.session.user = user._id
+            res.redirect('/')
+        })        
+        }
+    })
+});    
 
-// /* GET home page. */
-// router.get('/', function(req, res, next) {
-//     res.cookie('greeting', 'Hi!!!').render('index', { title: 'Express', menu:menu });
-// });
 
 
 module.exports = router;
