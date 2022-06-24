67bc8f10ee28537ea97bc640e44ff8152bcd06be
Ibragimov Eldar <90416969+ibragimoov@users.noreply.github.com>
2022-06-24 19:21:13 +0300

    heroes/

--- all/routes/heroes.js
+++ all/routes/heroes.js
@@ -1,14 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var Hero = require("../models/hero").Hero
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Новый маршрутизатор, для маошрутов, начинающихся с heroes');
+  res.send('Новый маршрутизатор, для маршрутов, начинающихся с heroes');
 });
 
 /* Страница героев */
-router.get("/:nick", function(req, res, next) {
-    res.send(req.params.nick);
-});
+router.get('/:nick', function(req, res, next) {
+    Hero.findOne({nick:req.params['nick']}, function(err,hero){
+        if(err) return next(err)
+        if(!hero) return next(new Error("Нет такого героя"))
+        res.render('hero', {
+            title: hero.title,
+            picture: '/' + hero.avatar,
+            desc: hero.desc
+        })
+    })
+})
 
 module.exports = router;
